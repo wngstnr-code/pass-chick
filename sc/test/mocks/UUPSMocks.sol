d7f289c20dd10893bcd36509ebbf26851a6f7abb
Diaz Amantajati Susilo <diazamantajatisusilo@mail.ugm.ac.id>
2026-04-25 14:15:27 +0700

    test(sc): add foundry tests

---
+++ sc/test/mocks/UUPSMocks.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {GameUSDC} from "../../src/GameUSDC.sol";
+import {USDCFaucet} from "../../src/USDCFaucet.sol";
+import {GameVault} from "../../src/GameVault.sol";
+import {GameSettlement} from "../../src/GameSettlement.sol";
+
+contract GameUSDCV2 is GameUSDC {
+    function version() external pure returns (uint256) {
+        return 2;
+    }
+}
+
+contract USDCFaucetV2 is USDCFaucet {
+    function version() external pure returns (uint256) {
+        return 2;
+    }
+}
+
+contract GameVaultV2 is GameVault {
+    function version() external pure returns (uint256) {
+        return 2;
+    }
+}
+
+contract GameSettlementV2 is GameSettlement {
+    function version() external pure returns (uint256) {
+        return 2;
+    }
+}
